8ef7898a1a526dbceaa1ad475d374bfebe444afc
SaiKiran Panuganti <saikiranpanuganti92@gmail.com>
2023-02-24 19:17:40 +0530

    Initial Commit

---
+++ AAADuty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AAADuty
+//
+//  Created by Saikiran Panuganti on 24/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
